0cc981077c976f2fb24cb5768915e9381d38d738
Youssef Hebish <youssefhebish27@gmail.com>
2026-07-07 18:10:14 +0300

    Update3Button

--- src/test/java/Pages/FlightsandHotelPage.java
+++ src/test/java/Pages/FlightsandHotelPage.java
@@ -5,5 +5,7 @@ import org.openqa.selenium.By;
 public class FlightsandHotelPage {
 
     private final By SearchButton = By.xpath("//div[@class='home-search-form-submit search-form-submit flights fh']");
+    private final By Search = By.xpath("//div[@class='home-search-form-submit search-form-submit flights fh']");
+    private final By Search_B = By.xpath("//div[@class='home-search-form-submit search-form-submit flights fh']");
 
 }
