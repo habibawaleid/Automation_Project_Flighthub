28f17e99b3ac86f64b251577739ab0f126b1b9f3
Youssef Hebish <youssefhebish27@gmail.com>
2026-07-07 17:55:38 +0300

    SearchButton

--- src/test/java/Pages/FlightsandHotelPage.java
+++ src/test/java/Pages/FlightsandHotelPage.java
@@ -1,4 +1,9 @@
 package Pages;
 
+import org.openqa.selenium.By;
+
 public class FlightsandHotelPage {
+
+    private final By SearchButton = By.xpath("//div[@class='home-search-form-submit search-form-submit flights fh']");
+
 }
