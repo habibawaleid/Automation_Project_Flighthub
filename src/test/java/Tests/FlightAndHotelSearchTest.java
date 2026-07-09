0a1ef603d7f6cdea49f786ccb0e19ec62204860b
Youssef Hebish <youssefhebish27@gmail.com>
2026-07-09 19:12:43 +0300

    Refactor Flights and Hotels feature into a separate Page Object class

--- src/test/java/Tests/FlightAndHotelSearchTest.java
+++ src/test/java/Tests/FlightAndHotelSearchTest.java
@@ -5,12 +5,9 @@ import com.github.javafaker.Faker;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 @Epic("Flights and Hotels")
 @Feature("Search Form Functionality")
@@ -21,7 +18,7 @@ public class FlightAndHotelSearchTest extends BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        homePage = new HomePage(driver,new WebDriverWait(driver, Duration.ofSeconds(10)));
+        homePage = new HomePage(driver);
         faker = new Faker();
     }
 
