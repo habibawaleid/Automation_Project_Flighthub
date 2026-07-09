63324e322cf56ced7a1723ebaeaf30f8469d1d8f
Youssef Hebish <youssefhebish27@gmail.com>
2026-07-09 18:58:34 +0300

    Delete FlightsandHotelPage.java

--- src/test/java/Pages/FlightsandHotelPage.java
+++
@@ -1,9 +0,0 @@
-package Pages;
-
-import org.openqa.selenium.By;
-
-public class FlightsandHotelPage {
-
-    private final By SearchButton = By.xpath("//div[@class='home-search-form-submit search-form-submit flights fh']");
-
-}
